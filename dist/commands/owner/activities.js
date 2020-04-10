94dbf14482094f60b174094d047cc51011cb7f6b
AlexbavGamer <Alexsandrebr@hotmail.com.br>
2020-04-10 20:16:50 -0300

    dashboard update

--- dist/commands/owner/activities.js
+++ dist/commands/owner/activities.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const MaytrixXCommand_1 = require("../../domain/MaytrixXCommand");
 class ActivitiesCommand extends MaytrixXCommand_1.MaytrixXCommand {
     constructor(client) {
@@ -11,8 +20,46 @@ class ActivitiesCommand extends MaytrixXCommand_1.MaytrixXCommand {
         });
     }
     run(message, level, args, flags) {
-        let flag = flags[0];
-        message.channel.send(flag);
+        var _a, _b;
+        return __awaiter(this, void 0, void 0, function* () {
+            let flag = flags[0];
+            switch (flag) {
+                case "add":
+                    {
+                        let input = args.join(" ");
+                        if (input.length == 0)
+                            return message.reply(`enter a input`);
+                        (_a = this.client.config.activities) === null || _a === void 0 ? void 0 : _a.push(input);
+                        message.channel.send(`Activitie added: ${input.Truncate(30, "end", "...")}`);
+                        break;
+                    }
+                case "remove":
+                    {
+                        let index = parseInt(args[0]);
+                        if (!index)
+                            return message.reply(`enter a index`);
+                        let selectedAct = this.client.config.activities[index];
+                        let response = yield this.client.awaitReply(message, `Are you sure you want to permanently delete ${selectedAct} from activities?`);
+                        if (["y", "yes"].includes(response.toString())) {
+                            delete this.client.config.activities[index];
+                            message.channel.send(`Activitie removed: ${selectedAct.Truncate(30, "end", "...")}`);
+                        }
+                        else if (["n", "no"].includes(response.toString())) {
+                            message.channel.send("Action canceled");
+                        }
+                        break;
+                    }
+                case "list":
+                    {
+                        let output = `= Activities List =\n\n\n`;
+                        (_b = this.client.config.activities) === null || _b === void 0 ? void 0 : _b.forEach((value, index) => __awaiter(this, void 0, void 0, function* () {
+                            output += `${value} == ${index}`;
+                        }));
+                        message.channel.send(output, { code: "asciidoc" });
+                        break;
+                    }
+            }
+        });
     }
 }
 module.exports = ActivitiesCommand;
