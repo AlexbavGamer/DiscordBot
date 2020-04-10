51618aa1401a42a7b575b3d97a0059f0fccf8fc6
AlexbavGamer <Alexsandrebr@hotmail.com.br>
2020-04-10 18:24:38 -0300

    dashboard update

--- dist/commands/owner/shutdown.js
+++ dist/commands/owner/shutdown.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const MaytrixXCommand_1 = require("../../domain/MaytrixXCommand");
 class ShutdownCommand extends MaytrixXCommand_1.MaytrixXCommand {
     constructor(client) {
@@ -10,6 +19,13 @@ class ShutdownCommand extends MaytrixXCommand_1.MaytrixXCommand {
         });
     }
     run(message, level, ...args) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.client.commands.forEach((cmd) => __awaiter(this, void 0, void 0, function* () {
+                yield this.client.unloadCommand(cmd.conf.name);
+            }));
+            message.channel.send(`Bot shutting down!`);
+            process.exit(1);
+        });
     }
 }
 module.exports = ShutdownCommand;
